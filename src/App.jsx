7c1fc9405bd319631dbd0b5b1c1725af6aa699be
axmadboy033-create <axmadboy033@gmail.com>
2026-07-02 11:27:16 +0500

    salom

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 
 import React from 'react'
-import Home from './Componet/pages/Home/Home'
-import Section1 from './Componet/pages/Section1/Section1'
-import Dashboard from './Componet/Dashboard/pages/Dashboard'
-import Chat from './Componet/Chat/pages/Chat'
-import Settings from './Componet/Settings/pages/Settings'
+import Header from './Header/Header'
 
 const App = () => {
   return (
     <div>
-       <Home />    
-      <Section1 />   
-      <Dashboard />
-      <Chat />
-      <Settings />
+      <Header/>
     </div>
   )
 }
 
 export default App
-
-
-
-// import Router from "./Router";
-
-// function App() {
-//   return <Router />;
-// }
-
-// export default App;
